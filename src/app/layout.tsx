a449b433c4c221609e3a5be44253e8efb6e9e4f3
RuiOkazaki <okazaki200226@gmail.com>
2023-10-09 03:21:36 +0900

    feat: favicon を設定した

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,24 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className={LINESeedJP.className}>
         <CustomMantineProvider>{children}</CustomMantineProvider>
