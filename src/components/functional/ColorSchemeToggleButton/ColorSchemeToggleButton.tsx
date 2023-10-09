ad7429a7414d46e64378dc82c6d9babe0176b31c
RuiOkazaki <okazaki200226@gmail.com>
2023-10-09 21:12:55 +0900

    feat: カラーモードを切り替えるボタンを実装

---
+++ src/components/functional/ColorSchemeToggleButton/ColorSchemeToggleButton.tsx
@@ -0,0 +1,44 @@
+'use client';
+
+import { ActionIcon, useMantineColorScheme } from '@mantine/core';
+import { IconSun, IconMoon, IconDeviceDesktop } from '@tabler/icons-react';
+import { FC, useEffect, useState } from 'react';
+
+const colorSchemeIconMapping = {
+  light: <IconSun />,
+  dark: <IconMoon />,
+  auto: <IconDeviceDesktop />,
+} as const;
+
+export const ColorSchemeToggleButton: FC = () => {
+  const { setColorScheme, colorScheme } = useMantineColorScheme();
+
+  const colorSchemeToggle = () => {
+    switch (colorScheme) {
+      case 'light':
+        setColorScheme('dark');
+        break;
+      case 'dark':
+        setColorScheme('auto');
+        break;
+      case 'auto':
+        setColorScheme('light');
+        break;
+    }
+  };
+
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+
+  return (
+    <ActionIcon
+      variant="subtle"
+      h={36}
+      w={36}
+      onClick={colorSchemeToggle}
+      loading={!mounted}
+    >
+      {mounted && colorSchemeIconMapping[colorScheme]}
+    </ActionIcon>
+  );
+};
