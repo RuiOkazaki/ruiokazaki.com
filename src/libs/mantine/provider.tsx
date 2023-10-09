d7b468b3328c01dcd334ee0891ebe1d7b0d3230a
RuiOkazaki <okazaki200226@gmail.com>
2023-10-10 02:44:41 +0900

    chore: toast 表示をするために @mantine/notification を導入

--- src/libs/mantine/provider.tsx
+++ src/libs/mantine/provider.tsx
@@ -1,4 +1,5 @@
 import { MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
 import { FC, ReactNode } from 'react';
 
 import { theme } from './theme';
@@ -8,5 +9,16 @@ type Props = {
 };
 
 export const CustomMantineProvider: FC<Props> = ({ children }) => (
-  <MantineProvider theme={theme}>{children}</MantineProvider>
+  <MantineProvider theme={theme}>
+    <Notifications
+      portalProps={{
+        style: {
+          position: 'fixed',
+          right: 0,
+          bottom: 0,
+        },
+      }}
+    />
+    {children}
+  </MantineProvider>
 );
