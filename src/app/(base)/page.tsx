8919a68c1c76ac122748cb5ed6f7156098bfce21
RuiOkazaki <okazaki200226@gmail.com>
2023-10-10 02:44:20 +0900

    feat: 自己紹介文を記載

--- src/app/(base)/page.tsx
+++ src/app/(base)/page.tsx
@@ -1,4 +1,4 @@
-import { Center, Flex, Stack } from '@mantine/core';
+import { Center, Flex, Space, Stack, Text } from '@mantine/core';
 import { NextPage } from 'next';
 
 import { Myself } from './_components/Myself';
@@ -8,6 +8,18 @@ const page: NextPage = () => {
     <Center h={'100%'} w={'100%'}>
       <Flex justify={'space-between'} w={'90%'}>
         <Stack justify={'center'} gap={0}></Stack>
+        <Text component="h1" fz={'1.75rem'} lh={'2.5'}>
+          Hi, I&apos;m{' '}
+          <Text component="span" fz={'inherit'} fw={'bold'}>
+            RuiOkazaki 🐱
+          </Text>
+          <br />A{' '}
+          <Text component="span" fz={'inherit'} fw={'bold'}>
+            &lt;／&gt; Frontend Developer
+          </Text>
+        </Text>
+        <Text>Make computer interfaces better for the humanities 🛠️</Text>
+        <Space h={24} />
         <Myself w={500} h={500} />
       </Flex>
     </Center>
